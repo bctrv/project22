6c9c7a9eb1637cd8daaa984a3d9507eadb9dbc4e
artem bystrov <ockidocki1@gmail.com>
2023-06-07 22:08:57 +0400

    test added

--- src/test/java/BankTestClass.java
+++ src/test/java/BankTestClass.java
@@ -7,10 +7,10 @@ import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
 
 public class BankTestClass {
 
@@ -28,6 +28,19 @@ public class BankTestClass {
         $("[placeholder='Город']").setValue("Петрозаводск");
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
         $("[data-test-id='date'] input").setValue(generateDate(3, "dd.MM.YYYY"));
+        $("[name='name']").setValue("Сергей Петров");
+        $("[name='phone']").setValue("+79123456789");
+        $("[data-test-id='agreement']").click();
+        $(byText("Забронировать")).click();
+        $(byText("Успешно!")).shouldBe(visible, Duration.ofMillis(11000));
+    }
+
+    @Test
+    void cityDateTest() {
+        $("[placeholder='Город']").setValue("Пе");
+        $(byText("Петрозаводск")).click();
+        $("[data-test-id='date']").click();
+        $(byText("30")).click();
         $("[name='name']").setValue("Сергей Петров");
         $("[name='phone']").setValue("+79123456789");
         $("[data-test-id='agreement']").click();
