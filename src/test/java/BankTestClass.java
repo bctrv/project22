f1f2ccad10607a4f1fd8edded7d04b973b775794
artem bystrov <ockidocki1@gmail.com>
2023-06-09 18:44:07 +0400

    test updated

--- src/test/java/BankTestClass.java
+++ src/test/java/BankTestClass.java
@@ -7,8 +7,7 @@ import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -25,28 +24,38 @@ public class BankTestClass {
 
     @Test
     void positiveTest() {
-        $("[placeholder='Город']").setValue("Петрозаводск");
+        String planningDate = generateDate(3, "dd.MM.YYYY");
+        $("[data-test-id='city'] input").setValue("Петрозаводск");
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
-        $("[data-test-id='date'] input").setValue(generateDate(3, "dd.MM.YYYY"));
+        $("[data-test-id='date'] input").setValue(planningDate);
         $("[name='name']").setValue("Сергей Петров");
         $("[name='phone']").setValue("+79123456789");
         $("[data-test-id='agreement']").click();
         $(byText("Забронировать")).click();
-        $(byText("Успешно!")).shouldBe(visible, Duration.ofMillis(11000));
+        $(".notification__content")
+                .shouldHave(text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
     }
 
     @Test
     void cityDateTest() {
-        $("[placeholder='Город']").setValue("Пе");
-        $(byText("Петрозаводск")).click();
-        $("[data-test-id='date']").click();
-        $(byText(generateDate(7, "dd"))).click();
+        String city = "Петрозаводск";
+        int daysToAdd = 7;
+        int defaultDaysAdded = 3;
+        $("[data-test-id='city'] input").setValue("Пе");
+        $$("[class=menu-item__control]").findBy(text(city)).click();
+        $("[data-test-id='date'] input").click();
+        $$("[class=calendar__day]");
+        if (!generateDate(defaultDaysAdded, "MM").equals(generateDate(daysToAdd, "MM"))) {
+            $("[data-step='1']").click();
+        }
+        $$(".calendar__day").findBy(text(generateDate(daysToAdd, "d"))).click();
         $("[name='name']").setValue("Сергей Петров");
         $("[name='phone']").setValue("+79123456789");
         $("[data-test-id='agreement']").click();
         $(byText("Забронировать")).click();
-        $(byText("Успешно!")).shouldBe(visible, Duration.ofMillis(11000));
+        $(".notification__content")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + generateDate(daysToAdd, "dd.MM.yyyy")));
     }
-
-
 }
